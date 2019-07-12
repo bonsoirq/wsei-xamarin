f26602d247b7ee150403fc7b3eeb96f3077e3709
Jarosław Bąk <jaroslaw.bak24@gmail.com>
2019-07-12 19:00:43 +0200

    Create Xamarin.Forms solution

---
+++ Todorin/Todorin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Todorin.Models;
+
+namespace Todorin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
